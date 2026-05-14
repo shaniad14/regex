e6f5ef29e1807bc671e8930f723245f979e3a11c
shaniad14 <shaniad1414@gmail.com>
2026-05-13 19:48:55 -0700

    Started the email part

--- main.py
+++ main.py
@@ -16,3 +16,15 @@ phoneRegex = re.compile(r'''
     \d{4}                   # last 4 digits
 )
 ''', re.VERBOSE)
+
+#  EMAIL REGEX 
+# This regex looks for email addresses
+
+emailRegex = re.compile(r'''
+(
+    [a-zA-Z0-9._%+-]+       # username
+    @                       # @ symbol
+    [a-zA-Z0-9.-]+          # domain name
+    \.[a-zA-Z]{2,}          # .com .ca .org etc.
+)
+''', re.VERBOSE)
