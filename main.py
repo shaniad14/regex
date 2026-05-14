139b8aafe3cee7e23bd3952bed67a1729e2c646a
shaniad14 <shaniad1414@gmail.com>
2026-05-13 19:43:22 -0700

    Added the phone number part

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ import pyperclip
 
 # Get text from clipboard
 text = pyperclip.paste()
+
+ #PHONE NUMBER REGEX 
+
+phoneRegex = re.compile(r'''
+(
+    (\d{3}|\(\d{3}\))?      # area code
+    (\s|-)?                 # separator
+    \d{3}                   # first 3 digits
+    (\s|-)                  # separator
+    \d{4}                   # last 4 digits
+)
+''', re.VERBOSE)
